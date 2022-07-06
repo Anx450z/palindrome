f685fffd22af6e15e417aa602a5cddcf434b256b
Anx450z <anx450z@gmail.com>
2022-07-06 14:53:09 +0530

    Add pakage jason and read me files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+Hi everyone, this is a learning project about TDD(test driven development) in JavaScript.
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
+module.exports = Phase;
 //native object modification
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
 
-// Returns true if the phrase is a palindrome, false otherwise
 function Phrase(content){
   this.content = content;
 
@@ -16,7 +15,8 @@ function Phrase(content){
     return this.processor(this.content);
   }
 
-  this.palindrome3 = function palindrome3() {
+  // Returns true if the phrase is a palindrome, false otherwise
+  this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
   }
 
@@ -24,15 +24,3 @@ function Phrase(content){
     return this.content.toUpperCase();
   }
 }
-
-function TranslatePhrase(content, translation){
-  this.content = content;
-  this.translation = translation;
-
-  // Return translation processed for palindrome testing.
-  this.processedContent = function processedContent(){ // overriding
-    return this.translation;
-  }
-}
-
-TranslatePhrase.prototype = new Phrase(); // prototype inherits methods of Phrase2
